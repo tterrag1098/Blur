8ed5a99466ce51f75e3b6481b27070e85c2db7c6
tterrag1098 <tterrag1098@gmail.com>
2017-05-24 23:41:13 -0400

    Make config GUI see-through when in a world

--- src/main/java/com/tterrag/blur/config/BlurConfigGui.java
+++ src/main/java/com/tterrag/blur/config/BlurConfigGui.java
@@ -1,15 +1,38 @@
 package com.tterrag.blur.config;
 
+import javax.annotation.Nonnull;
+
 import com.tterrag.blur.Blur;
 
 import net.minecraft.client.gui.GuiScreen;
+import net.minecraft.client.renderer.Tessellator;
 import net.minecraftforge.common.config.ConfigElement;
 import net.minecraftforge.common.config.Configuration;
 import net.minecraftforge.fml.client.config.GuiConfig;
+import net.minecraftforge.fml.client.config.GuiConfigEntries;
 
 public class BlurConfigGui extends GuiConfig {
 
     public BlurConfigGui(GuiScreen parentScreen) {
         super(parentScreen, new ConfigElement(Blur.instance.config.getCategory(Configuration.CATEGORY_GENERAL)).getChildElements(), Blur.MODID, false, false, Blur.MODID + ".config.title");
+    }
+    
+    @Override
+    public void initGui() {
+        super.initGui();
+        this.entryList = new GuiConfigEntries(this, mc) {
+            @SuppressWarnings({ "unused", "null" })
+            @Override
+            protected void drawContainerBackground(@Nonnull Tessellator tessellator) {
+                if (mc.world == null) {
+                    super.drawContainerBackground(tessellator);
+                }
+            }
+        };
+    }
+    
+    @Override
+    public void drawDefaultBackground() {
+        drawWorldBackground(0);
     }
 }
